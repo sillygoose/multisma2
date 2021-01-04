88617e8434917fe958d6777d74879f6ea3c083bb
Rick Naro <sillygoose@me.com>
2021-01-03 22:45:46 -0500

    caching so don't send unchanged outputs

--- influx.py
+++ influx.py
@@ -28,29 +28,24 @@ class InfluxDB():
             self._client.close()
             logger.info(f"Closed the InfluxDB database '{INFLUXDB_DATABASE}'")
 
+    cache = {}
+
     def write(self, points):
         if not self._client:
             return
-        #pprint(points)
+
         ts = time.time_ns()
         lps = []
         for point in points:
-            precision = point.pop('precision', None)
-            unit = point.pop('unit', None)
+            point.pop('precision', None)
+            point.pop('unit', None)
             topic = point.pop('topic')
             topic = topic.replace('/', '_')
-            #pprint(point)
-            #"airSensors,sensor_id=TLM0#{v[:id]} temperature=#{v[:t]},humidity=#{v[:h]},co=#{v[:c]}"
-            #migration,id=91752A,s2_cell_id=164b35c lat=8.3495,lon=39.01233 1554123600000000000
-            #migration,id=91752A,s2_cell_id=164b3dc lat=8.56067,lon=39.08883 1554102000000000000
-            #migration,id=91752A,s2_cell_id=17b4854 lat=7.86233,lon=38.81167 1547557200000000000
             lp = f"{topic} "
             first = True
             for k, v in point.items():
-                #print(f"{k}  {v}")
                 if isinstance(v, dict):
                     for sk, sv in v.items():
-                        #print(f"{sk}  {sv}")
                         if first:
                             lp += f"{k}_{sk}={sv}i"
                             first = False
@@ -69,10 +64,20 @@ class InfluxDB():
                         first = False
                     else:
                         lp += f",{k}={prefix}{v}{suffix}"
+
+            # Check if in cache, if not or different update cache and output
+            cached_result = InfluxDB.cache.get(topic, None)
+            if cached_result:
+                cached_lp = InfluxDB.cache.get(topic)
+                if lp == cached_lp:
+                    continue
+
+            InfluxDB.cache[topic] = lp
             lp += f" {ts}"
             lps.append(lp)
-        #pprint(lps)
-        result = self._client.write_points(lps, protocol='line')
-        #print(result)
-        if not result:
-            logger.error(f"Database write failed")
+
+        if len(lps):
+            #pprint(lps)
+            result = self._client.write_points(point=lps, time_precision='s', protocol='line')
+            if not result:
+                logger.error(f"Database write_points() failed")
